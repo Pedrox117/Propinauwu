02cfdbf8e476e6fa39cc4184f47484fb2ae2ccc0
pedrox117 <pedroquen64@gmail.com>
2023-11-29 04:02:19 -0600

    segundo commit

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
   const [billAmount, setBillAmount] = useState('');
   const [tipPercentage, setTipPercentage] = useState(15);
   const [numberOfPeople, setNumberOfPeople] = useState('1');
   const [total, setTotal] = useState(0);
+  const [tipAmount, setTipAmount] = useState(0);
+  const [totalWithTip, setTotalWithTip] = useState(0);
 
   const calculateTip = () => {
     const bill = parseFloat(billAmount) || 0;
     const tip = (bill * tipPercentage) / 100;
     const totalAmount = bill + tip;
+
+    setTipAmount(tip.toFixed(2));
+    setTotalWithTip(totalAmount.toFixed(2));
+
     const perPersonTotal = totalAmount / parseInt(numberOfPeople);
     setTotal(perPersonTotal.toFixed(2));
+  };
+
+  const goToResultScreen = () => {
+    const peopleArray = Array.from({ length: parseInt(numberOfPeople) }, (_, index) => ({
+      id: index.toString(),
+      totalPerPerson: total,
+    }));
+
+    navigation.navigate('Result', {
+      peopleArray,
+    });
   };
 
   return (
@@ -52,25 +73,90 @@ export default function App() {
       <Button
         title="Calcular Propina"
         onPress={calculateTip}
-        color="#BA68C8" // color morado claro
+        color="#BA68C8" 
       />
+      <Text style={styles.resultText}>Importe de Propina: ${tipAmount}</Text>
+      <Text style={styles.resultText}>Importe Total: ${totalWithTip}</Text>
       <Text style={styles.total}>Total por persona: ${total}</Text>
+      <Button
+        title="Ver Desglose"
+        onPress={goToResultScreen}
+        color="#BA68C8" 
+      />
       <StatusBar style="auto" />
     </View>
+  );
+};
+
+const ResultScreen = ({ route, navigation }) => {
+  const { peopleArray } = route.params;
+  const [selectedPerson, setSelectedPerson] = useState(null);
+  const [paidAmount, setPaidAmount] = useState('');
+
+  const calculateChange = () => {
+    if (selectedPerson !== null) {
+      const paid = parseFloat(paidAmount) || 0;
+      const totalPerPerson = peopleArray[selectedPerson].totalPerPerson;
+      const change = paid - totalPerPerson;
+      alert(`Cambio: $${change.toFixed(2)}`);
+    } else {
+      alert('Selecciona a una persona antes de calcular el cambio.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Desglose de la Cuenta</Text>
+      <FlatList
+        data={peopleArray}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => setSelectedPerson(index)}
+            style={[
+              styles.personItem,
+              { backgroundColor: selectedPerson === index ? '#BA68C8' : '#FFD9E6' },
+            ]}
+          >
+            <Text style={styles.personText}>Persona {index + 1}: ${item.totalPerPerson}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Monto Pagado"
+        keyboardType="numeric"
+        value={paidAmount}
+        onChangeText={(text) => setPaidAmount(text)}
+      />
+      <Button title="Calcular Cambio" onPress={calculateChange} color="#BA68C8" />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFD9E6', // fondo rosado claro
+    backgroundColor: '#FFD9E6', 
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 18,
     marginBottom: 10,
-    color: '#000', // texto negro
+    color: '#000', 
   },
   inputContainer: {
     flexDirection: 'row',
@@ -79,31 +165,46 @@ const styles = StyleSheet.create({
   currencySymbol: {
     fontSize: 18,
     marginRight: 5,
-    color: '#000', // texto negro
+    color: '#000', 
   },
   percentSymbol: {
     fontSize: 18,
     marginRight: 5,
-    color: '#000', // texto negro
+    color: '#000', 
   },
   personSymbol: {
     fontSize: 18,
     marginRight: 5,
-    color: '#000', // texto negro
+    color: '#000', 
   },
   input: {
     height: 40,
-    borderColor: '#000', // borde negro
+    borderColor: '#000',
     borderWidth: 1,
     margin: 10,
     width: 200,
     textAlign: 'center',
-    color: '#000', // texto negro
+    color: '#000', 
   },
   total: {
     fontSize: 18,
     marginTop: 10,
-    color: '#000', // texto negro
+    color: '#000',
+    textAlign: 'right', 
+  },
+  resultText: {
+    fontSize: 18,
+    marginTop: 5,
+    color: '#000', 
+    textAlign: 'right',
+  },
+  personItem: {
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  personText: {
+    fontSize: 16,
+    color: '#000',
   },
 });
-
